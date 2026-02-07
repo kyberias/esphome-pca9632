0c49bbd5cf0652ef99b21251d2893c1dcb273dc5
Jaakko Haakana <jaakko@haakana.net>
2026-02-07 18:32:38 +0200

    Fix init

--- components/pca9632/__init__.py
+++ components/pca9632/__init__.py
@@ -1,23 +1,24 @@
-
-## `components/pca9632/__init__.py`
-```python
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c
-from esphome.const import CONF_ID, CONF_ADDRESS
+from esphome.const import CONF_ID
 
 CODEOWNERS = ["@kyberias"]
 DEPENDENCIES = ["i2c"]
 
 pca9632_ns = cg.esphome_ns.namespace("pca9632")
-PCA9632Component = pca9632_ns.class_("PCA9632Component", cg.Component, i2c.I2CDevice)
+PCA9632Component = pca9632_ns.class_(
+    "PCA9632Component", cg.Component, i2c.I2CDevice
+)
 
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.Required(CONF_ID): cv.declare_id(PCA9632Component),
-        cv.Optional(CONF_ADDRESS, default=0x60): cv.i2c_address,
-    }
-).extend(i2c.i2c_device_schema(0x60))
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.Required(CONF_ID): cv.declare_id(PCA9632Component),
+        }
+    )
+    .extend(i2c.i2c_device_schema(0x60))  # provides `address:` correctly
+)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
